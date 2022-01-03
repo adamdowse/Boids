cdb90b6b3ce464c81501140828b5ec22b33972df
Adam Dowse <adamdowse@hotmail.co.uk>
2022-01-03 15:46:56 +0000

    Text in the center of screen

--- boids.py
+++ boids.py
@@ -2,3 +2,20 @@
 import pyglet
 
 
+window = pyglet.window.Window()
+
+#Draw some text in the center of the screen
+label = pyglet.text.Label('Hello World',
+    font_name='Times New Roman',
+    font_size=36,
+    x=window.width//2, y=window.height//2,
+    anchor_x='center', anchor_y='center')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
+
+pyglet.app.run()
